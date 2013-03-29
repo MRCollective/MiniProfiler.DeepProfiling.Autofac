b2ba46afef3b8cb52e4537dcd5276ef2bbce5b6c
Matt Davies <matthew.davies@curtin.edu.au>
2013-03-29 19:13:05 +0800

    Initial commit

---
+++ MiniProfiler.DeepProfiling.Autofac/MiniProfilingInterceptionModule.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Linq;
+using Autofac;
+using Autofac.Core;
+using Castle.DynamicProxy;
+
+namespace StackExchange.Profiling.DeepProfiling.Autofac
+{
+    public class MiniProfilerInterceptionModule : Module
+    {
+        protected override void AttachToComponentRegistration(IComponentRegistry componentRegistry, IComponentRegistration registration)
+        {
+            registration.Activating += (sender, e) =>
+            {
+                //Todo: Unit tests
+                //Todo: Ensure this works for special cases: IEnumerable, Func etc
+                var serviceType = ((TypedService)registration.Services.First()).ServiceType;
+                if (!serviceType.IsInterface || !serviceType.IsVisible || serviceType.IsAssignableTo<IEnumerable>() || e.Instance is IProxyTargetAccessor)
+                {
+                    return;
+                }
+
+                e.Instance = new ProxyGenerator().CreateInterfaceProxyWithTarget(serviceType, e.Instance, new ProfilingInterceptor());
+            };
+
+            base.AttachToComponentRegistration(componentRegistry, registration);
+        }
+    }
+}
---
+++ MiniProfiler.DeepProfiling.Autofac/ProfilingInterceptor.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Castle.DynamicProxy;
+
+namespace StackExchange.Profiling.DeepProfiling.Autofac
+{
+    internal class ProfilingInterceptor : IInterceptor
+    {
+        public void Intercept(IInvocation invocation)
+        {
+            var parameters = string.Join(", ", invocation.Arguments.Select(a => (a ?? "").ToString()).ToArray());
+
+            using (MiniProfiler.Current.Step(string.Format("Method {0} ({1})\nParams {2}", invocation.Method.Name, invocation.InvocationTarget.GetType().Name, parameters)))
+            {
+                invocation.Proceed();
+            }
+        }
+    }
+}
